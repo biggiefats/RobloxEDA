e2854fac1128541eb20b5874a5189b5ebce0fcb6
biggiefats <114266314+biggiefats@users.noreply.github.com>
2024-09-29 02:36:09 +0100

    Added main.py for future editing

---
+++ main.py
@@ -0,0 +1,36 @@
+# Roblox EDA
+
+"""
+Questions/Hypotheses to explore:
+
+Games that include a reference to an update
+in their title will have a greater ratio
+of current players to total players than games
+that do not.
+
+There is a quadratic proportion between one
+of two variables in this dataset.
+(Linear Regression / Poly Features)
+
+Is the length of a title an indicator of the rating
+of the game above 90%?
+(Logistic Regression)
+
+Are there common words used in games that have a high
+all-time visits and a high favourites count?
+(Clustering)
+
+What games have disproportionate like and dislike counts,
+given their ratio?
+(DBSCAN)
+
+Do the likes, dislikes and total visits indicate whether
+or not a game's current players is above 10% of the number
+of favourites on game?
+(Naive Bayes)
+
+Classify games by how many UNIQUE letters they have.
+Does the general attributes of success to a game define
+the unique letters in the game?
+(Decision Tree Classifier)
+"""
